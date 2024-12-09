58d70fc5ca54fdbd229209ff5b8b2bd2abfcdfd5
nomanmaniyar <nomaanmaniyar123@gmail.com>
2024-12-09 23:40:15 +0530

    update: expnse controller

--- src/main/java/com/dailyexpense/controller/ExpenseController.java
+++ src/main/java/com/dailyexpense/controller/ExpenseController.java
@@ -3,14 +3,16 @@ package com.dailyexpense.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dailyexpense.dto.ExpenseDTO;
 import com.dailyexpense.service.ExpenseService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/api/expense")
@@ -25,10 +27,9 @@ public class ExpenseController {
     }
 
     @PostMapping
-    public ExpenseDTO savExpenseDTO(@RequestBody ExpenseDTO expenseDTO) {
-
-        return expenseService.savExpense(expenseDTO);
-
+    public ResponseEntity<ExpenseDTO> savExpenseDTO(@RequestBody ExpenseDTO expenseDTO) {
+        ExpenseDTO savedExpense = expenseService.savExpense(expenseDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedExpense);
     }
 
 }

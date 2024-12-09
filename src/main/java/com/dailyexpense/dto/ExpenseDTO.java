3fe8db7e26869bbaf9fb55ca913f7f8e04e88ce6
nomanmaniyar <nomaanmaniyar123@gmail.com>
2024-12-10 00:05:49 +0530

    add: DTO validation

--- src/main/java/com/dailyexpense/dto/ExpenseDTO.java
+++ src/main/java/com/dailyexpense/dto/ExpenseDTO.java
@@ -2,19 +2,30 @@ package com.dailyexpense.dto;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class ExpenseDTO {
 
     private long id;
-    
+    @NotNull(message = "Something is worng with the expenseType")
+    @Size(min = 1, max = 50)
     private String expenseType;
+    @NotNull
+    @Size(min = 1, max = 50)
     private String payee;
-    private String category;
+    @NotNull
+    @Size(min = 1, max = 50)
+    private String category;    
+    @NotNull
     private LocalDate date;
+    @Positive
     private Double amount;
     private Long paymentId; // Optional, can be null if creating a new payment
     private String paymentMode; // Required for new payment
     private String paymentDetails; // Required for new payment
-    
+
     public String getPaymentMode() {
         return paymentMode;
     }
